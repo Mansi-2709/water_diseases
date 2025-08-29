2c95c73c010a7fb3922510735a264b6bfa35f80d
Mansi Sharma <86704359+Mansi-2709@users.noreply.github.com>
2025-08-29 23:01:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Water Disease Prediction App')
 
 with st.expander('Data'):
   st.write('**Raw Data**')
-
+  df = pd.read_csv('https://github.com/Mansi-2709/water_diseases/blob/master/my_dataframe.csv')
+  df
 
 with st.expander('Input Data'):
   
