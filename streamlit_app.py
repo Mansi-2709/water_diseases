20900a30d49964d9724ef08a31ddc293521fcdc0
Mansi Sharma <86704359+Mansi-2709@users.noreply.github.com>
2025-08-29 23:42:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,9 @@ with st.expander('Data'):
   st.write('**Raw Data**')
   df = pd.read_csv('https://raw.githubusercontent.com/Mansi-2709/water_diseases/refs/heads/master/my_dataframe.csv')
   df
-  
+  st.write('**X**')
+  X = df.drop('Diarrheal Cases per 100,000 people',axis=1)
+  X
+  st.write('**y**')
+  y = df.Diarrheal Cases per 100,000 people
+  y
