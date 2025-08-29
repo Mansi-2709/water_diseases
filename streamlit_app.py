d7bf05a281efb51249eb3765b1aaca837c68b7c8
Mansi Sharma <86704359+Mansi-2709@users.noreply.github.com>
2025-08-29 23:03:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,4 @@ with st.expander('Data'):
   st.write('**Raw Data**')
   df = pd.read_csv('https://github.com/Mansi-2709/water_diseases/blob/master/my_dataframe.csv')
   df
-
-with st.expander('Input Data'):
   
