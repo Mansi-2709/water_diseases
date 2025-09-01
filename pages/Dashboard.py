020cfdb9d7c7db289c36406cff9042dc31086d19
Mansi Sharma <86704359+Mansi-2709@users.noreply.github.com>
2025-09-01 23:08:08 +0530

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+import os
+import warnings
+warnings.filterwarnings('ignore')
 
+st.set_page_config(page_title="Water Disease Dashboard", page_icon=":droplet:",layout="wide")
