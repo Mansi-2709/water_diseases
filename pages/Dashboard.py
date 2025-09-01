3b4dd7fa8161fd2d66ed65e841bf09b395cf98fe
Mansi Sharma <86704359+Mansi-2709@users.noreply.github.com>
2025-09-02 00:03:19 +0530

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -9,3 +9,7 @@ st.set_page_config(page_title="Water Disease Dashboard", page_icon=":droplet:",l
 df = pd.read_csv('https://raw.githubusercontent.com/Mansi-2709/water_diseases/refs/heads/master/water_pollution_disease.csv')
 
 st.sidebar.header("Choose your filter: ")
+region = st.sidebar.selectbox("Pick your Region", df["Region"].unique())
+country = st.sidebar.selectbox("Pick your Country", df["Country"].unique())
+source = st.sidebar.selectbox("Pick your Water Source", df["Water Source Type"].unique())
+treatment = st.sidebar.selectbox("Pick your Water Treatment", df["Water Treatment Method"].unique())
