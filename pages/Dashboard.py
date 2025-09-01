e7a45f43ab5c4b3686b6964ce74724fa4d313fbf
Mansi Sharma <86704359+Mansi-2709@users.noreply.github.com>
2025-09-01 23:38:19 +0530

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -6,3 +6,6 @@ import warnings
 warnings.filterwarnings('ignore')
 
 st.set_page_config(page_title="Water Disease Dashboard", page_icon=":droplet:",layout="wide")
+df = pd.read_csv('https://raw.githubusercontent.com/Mansi-2709/water_diseases/refs/heads/master/water_pollution_disease.csv')
+
+st.sidebar.header("Choose your filter: ")
